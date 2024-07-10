d72b37ce491d89a0779e2da88db413e761792bc5
Alfredolsl <122558734+Alfredolsl@users.noreply.github.com>
2024-07-10 20:38:46 +0000

    add login and menu routes

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,11 @@ from flask import current_app as app, render_template
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
