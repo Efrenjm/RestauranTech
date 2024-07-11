be645862bfb7be7e397f4a61c060ac1e70f12a61
Alfredolsl <122558734+Alfredolsl@users.noreply.github.com>
2024-07-11 20:27:08 +0000

    add register route and add documentation

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-#from flask import current_app as app, render_template
-from flask import render_template
+""" File containing all routes for the project """
 from app import app
-
-@app.route('/')
-def home():
-    return render_template('login.html')
+from app.forms import RegisterForm
+from app.user import User
+from flask import render_template, redirect, url_for
+from uuid import uuid4
+from werkzeug.security import generate_password_hash
 
 @app.route('/menu')
 def menu():
@@ -13,3 +13,23 @@ def menu():
 @app.route('/crm')
 def crm():
     return render_template('admin.html')
+
+@app.route('/login', methods=["GET", "POST"])
+def home():
+    return render_template('login.html')
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+
+    if form.is_submitted():
+        name = form.name.data
+        email = form.email.data
+        hashed_password = generate_password_hash(form.password.data,
+                                                 method="pbkdf2:sha256",
+                                                 salt_length=8)
+        new_user = User(name=name, email=email, password=hashed_password)
+        new_user.id = str(uuid4)
+        return redirect(url_for('crm'))
+
+    return render_template('register.html', form=form)
