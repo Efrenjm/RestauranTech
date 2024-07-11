477db3fc8700e9ab1cd86f4c8284618514ce0e8d
Alfredolsl <122558734+Alfredolsl@users.noreply.github.com>
2024-07-11 03:31:21 +0000

    switch template to render

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@ from app import app
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/menu')
 def menu():
-    return render_template('menu.html')
+    return render_template('index.html')
