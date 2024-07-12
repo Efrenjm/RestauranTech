81f9d4e69b6057a67f2aa0a403ce3dcf0245dc46
Alfredolsl <122558734+Alfredolsl@users.noreply.github.com>
2024-07-12 00:47:16 +0000

    add __tablename__ and documentation

--- app/user.py
+++ app/user.py
@@ -4,9 +4,9 @@ from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
     """ Represents user table for database 'restaurantech' """
-    today = date.today()
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64), nullable=False)
     email = db.Column(db.String(128), nullable=False, unique=True)
     password = db.Column(db.String(128), nullable=False)
-    created_at = db.Column(db.Date, default=today.strftime("%d-%m-%Y"))
+    created_at = db.Column(db.Date, default=date.today())
