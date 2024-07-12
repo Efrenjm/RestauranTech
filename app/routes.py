f1dd8a7d584eabf3a417b4e624b2f2270782b1e5
Alfredolsl <122558734+Alfredolsl@users.noreply.github.com>
2024-07-12 23:48:19 +0000

    add login functionality

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 """ File containing all routes for the project """
 from app import app, db
-from app.forms import RegisterForm
+from app.forms import RegisterForm, LoginForm
 from app.user import User
-from flask import render_template, redirect, url_for
-from werkzeug.security import generate_password_hash
+from flask import render_template, redirect, url_for, session
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route('/menu')
 def menu():
@@ -14,8 +14,27 @@ def crm():
     return render_template('admin.html')
 
 @app.route('/login', methods=["GET", "POST"])
-def home():
-    return render_template('login.html')
+def login():
+    form = LoginForm()
+
+    if form.is_submitted():
+        email = form.email.data
+        password = form.password.data
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            print("user doesn't exist")
+            return redirect(url_for("login"))
+        
+        elif not check_password_hash(user.password, password):
+            print("wrong password")
+            return redirect(url_for("login"))
+        
+        else:
+            session["iduser"] = user.id
+            return redirect(url_for("crm"))
+
+    return render_template('login.html', form=form)
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
@@ -24,7 +43,6 @@ def register():
     if form.is_submitted():
         email = form.email.data
         if not form.validate_email(email):
-            print("not duplicate!")
             name = form.name.data
             hashed_password = generate_password_hash(form.password.data,
                                                     method="pbkdf2:sha256",
