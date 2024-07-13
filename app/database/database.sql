76bd3925580ecf844ce620f41001439408e5ed53
Alfredolsl <122558734+Alfredolsl@users.noreply.github.com>
2024-07-13 00:33:38 +0000

    removed user, database creation and user privileges

--- app/database/database.sql
+++ app/database/database.sql
@@ -1,9 +1,3 @@
-CREATE DATABASE IF NOT EXISTS `restaurantech`;
-
-CREATE USER IF NOT EXISTS 'test'@'localhost' IDENTIFIED BY 'pwd';
-
-USE `restaurantech`;
-
 CREATE TABLE IF NOT EXISTS `users` (
     `id` INT AUTO_INCREMENT PRIMARY KEY,
     `name` varchar(128) NOT NULL,
@@ -11,6 +5,3 @@ CREATE TABLE IF NOT EXISTS `users` (
     `password` varchar(128) NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
-GRANT ALL PRIVILEGES ON restaurantech.* TO 'test'@'localhost';
-FLUSH PRIVILEGES;
