5ac662b6b0f373b361d1dd0741e09d4257aa4ebb
Alfredolsl <122558734+Alfredolsl@users.noreply.github.com>
2024-07-17 01:07:14 +0000

    added class modules from app/models

--- run.py
+++ run.py
@@ -1,5 +1,15 @@
-from flask import Flask
 from app import app
+from app.models.assets import Assets
+from app.models.branch import Branch
+from app.models.company import Company
+from app.models.footprint import Footprint
+from app.models.inventory import Inventory
+from app.models.order_details import Order_details
+from app.models.order import Order
+from app.models.role import Role
+from app.models.supplier import Supplier
+from app.models.user_to_branch import User_to_branch
+from app.models.user import User
 
 if __name__ == '__main__':
     app.run(debug=True)
