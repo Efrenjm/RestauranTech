05b2b0c8011e8eaaab264017d65853ef599dfd1c
Efrén Jiménez <40528724+Efrenjm@users.noreply.github.com>
2024-07-17 03:05:51 -0600

    add landing page

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,11 @@ from flask import render_template, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
 @app.route('/crm')
 def crm():
     return render_template('admin.html')
